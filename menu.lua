ddf96061e0e0ab56286238a1d8b21136033351eb
Sasha Moak <sasha.moak@gmail.com>
2019-02-15 16:10:46 -0800

    Getting rid of the games portion of the menu

--- menu.lua
+++ menu.lua
@@ -1,22 +1,13 @@
 local internetmenu = {
-  { "chromium", "chromium --incognito" },
+  { "chromium", "chromium" },
+  { "chromium (incognito)", "chromium --incognito" },
   { "firefox", "/usr/bin/firefox" },
-}
-
-local gamesmenu = {
-  { "steam", "/usr/bin/steam" },
-  { "Battle.net", "setarch i386 -L -B -R -3 /usr/bin/wine \"/home/smoak/.wine/drive_c/Program Files (x86)/Battle.net/Battle.net Launcher.exe\"" },
-  { "Guild Wars", "/usr/bin/wine \"/home/smoak/.wine/drive_c/Program Files (x86)/Guild Wars/Gw.exe\" -dx8 -dsound" },
-  { "Guild Wars 2", "/usr/bin/wine \"/home/smoak/.wine/drive_c/Program Files (x86)/Guild Wars 2/Gw2.exe\" -dx9single" },
-  { "minecraft", "/usr/bin/java -jar /home/smoak/games/Minecraft.jar" },
-  { "factorio", "factorio" }
 }
 
 local misc_menu = {
   { "keepass", "keepassx" },
-  { "gimp", "gimp" },
   { "IntelliJ", "idea.sh" },
-  { "Android Studio", "android-studio" }
+  { "VSCode", "code" },
 }
 
 local system_menu = {
@@ -26,7 +17,6 @@ local system_menu = {
 
 menu = {
   { "internet", internetmenu },
-  { "games", gamesmenu },
   { "misc", misc_menu },
   { "system", system_menu },
 }
